f9877a429ee9736dbb6e519d6d9e14a84db82e83
ngarland9 <nateg@pvep.com>
2020-11-19 20:48:13 -0500

    added win condition methods and play method loop. stuck on bug in make_move call for PrintBoard

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -3,22 +3,51 @@ LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 class Game
   def initialize()
     @board = Array.new(10)
-    @current_player = 0
-    @other_player = 0
-    @player1 = Player.new(1, "X")
-    @player2 = Player.new(2, "O")
+  
+    @player1 = HumanPlayer.new(1, "X")
+    @player2 = ComputerPlayer.new(2, "O")
+
+    @current_player = @player1
+    @other_player = @player2
   end
-  attr_reader :board, :current_player
+  attr_reader :board, :current_player, :PrintBoard
 
   def play()
     loop do
       place_marker(@current_player)
 
-
+      if player_has_won?(@current_player)
+        puts "#{@current_player} wins!"
+        print_board
+        return
+      elsif board_full?
+        puts "It's a draw."
+        print_board
+        return
+      end
+      switch_player!
+    end
   end
+
+
+  def player_has_won?(player)
+    LINES.any? do |line|
+      line.all? {|position| @board[position] == player.marker}
+    end
+  end
+  
+  def board_full?
+    free_positions.empty?
+  end
+
   def switch_player()
     @current_player = @other_player
   end
+
+  def free_positions
+    (1..9).select {|position| @board[position].nil?}
+  end
+
   def PrintBoard
     col_separator, row_separator = " | ", "--+---+--"
     label_for_position = lambda{|position| @board[position] ? @board[position] : position}
@@ -28,11 +57,23 @@ class Game
      rows_for_display = row_positions.map(&row_for_display)
      puts rows_for_display.join("\n" + row_separator + "\n")
   end
+
+
   def place_marker(player)
-    position = player.make_move
-    @board[position] = @current_player.marker
+    loop do 
+      position = player.make_move
+    if (1..9).include?(@board[position])
+      return @board[position] = @current_player.marker
+    else
+      puts "That square has already been taken please select another"
+      
+    end
+  end
   end
 end
+
+
+
 
 class Player < Game
   def initialize(turn, marker)
@@ -45,10 +86,24 @@ end
 
 class HumanPlayer < Player
   def make_move
-    Game.PrintBoard
+      puts @Game.PrintBoard
       print "Please select a square!"
       selection = gets.chomp.to_i
       selection
   
   end
 end
+
+
+class ComputerPlayer < Player
+  def make_move
+      selection = rand(1..9)
+      selection
+  
+  end
+end
+
+
+
+
+puts Game.new.play
