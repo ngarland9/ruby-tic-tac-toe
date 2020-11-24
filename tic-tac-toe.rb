b86a4d84ed6b936b3c20b88bcf673c81a747deb3
ngarland9 <nateg@pvep.com>
2020-11-24 14:17:02 -0500

    Added game logic still not perfect must improve recognition of win condition

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -5,18 +5,17 @@ WINS = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
 class Game
   def initialize(player1_sentience)
     @board = Array.new(10)
-    player1_sentience == "human" ? @player1 = HumanPlayer.new(1, "X","Human Player") : @player1 = ComputerPlayer.new(1, "X", "Computer X")
-    @player2 = ComputerPlayer.new(2, "O", "Computer O")
+    player1_sentience == "human" ? @player1 = HumanPlayer.new(1, "X", "Human Player", self) : @player1 = ComputerPlayer.new(1, "X", "Computer X", self)
+    @player2 = ComputerPlayer.new(2, "O", "Computer O", self)
     @markers = ["X", "O"]
     @current_player = @player1
     @other_player = @player2
   end
-  attr_reader :board, :current_player, :print_board, :free_positions
+  attr_reader :board, :current_player, :print_board
 
-  def play()
+  def play
     loop do
       print_board
-      puts 
       place_marker(@current_player)
 
       if player_has_won?(@current_player)
@@ -80,12 +79,13 @@ end
 
 
 class Player < Game
-  def initialize(turn, marker, name)
+  def initialize(turn, marker, name, game)
     @turn = turn
     @marker = marker
     @name = name
+    @game = game
   end
-  attr_reader :marker, :name
+  attr_reader :marker, :name 
 end
 
 
@@ -100,24 +100,62 @@ end
 
 
 class ComputerPlayer < Player
+  @@prior_selectionsX = []
+  @@prior_selectionsO = []
   def make_move
-    if @turn == 1
-      @turn += 1
-      selection = 5
-    elsif WINS.any? do |line|
-        selection = line.uniq[0] if line.uniq.length < line.length
-    end
-    else
-      selection = rand(1..9)
-    end
-      puts "\n#{@name} selects #{selection}"
-      selection.to_i
     
+    corners = [1,3,7,9]
+      if @turn == 1
+        @turn += 2
+        selection = corners.sample
+        @@prior_selectionsX << selection
+        selection 
+      elsif @turn == 2
+        @turn += 2
+        @@prior_selectionsO << 5
+        selection = 5
+      elsif @turn == 3
+        @turn += 2
+        last_move = @@prior_selectionsX[0].to_i
+        best_moves = {1=>[2,4], 3=>[2,6], 7=>[4,8], 9=>[6,8]}
+        selection = best_moves[last_move].sample
+        @@prior_selectionsX << selection
+        selection
+      elsif WINS.any? {|line| (line - @@prior_selectionsX).length == 1}
+        near_wins = WINS.map {|line| (line - @@prior_selectionsX)}
+        move = near_wins.select {|n| n.length == 1}
+        selection = move[0].sample
+          if (@game.free_positions).include?(selection)
+            @game.current_player.marker == "X" ? @@prior_selectionsX << selection : @@prior_selectionsO << selection
+          selection = move[0].sample
+          else
+            selection = @game.free_positions[rand(@game.free_positions.length)]
+          
+          end
+       
+      elsif WINS.any? {|line| (line - @@prior_selectionsO).length == 1}
+        near_wins = WINS.map {|line| line - @@prior_selectionsO}
+        move = near_wins.select {|n| n.length == 1}
+        selection = move[0].sample
+          if (@game.free_positions).include?(selection)
+            @game.current_player.marker == "X" ? @@prior_selectionsX << selection : @@prior_selectionsO << selection
+            selection = move[0].sample
+          else
+            selection = @game.free_positions[rand(@game.free_positions.length)]
+            
+          end
+      
+      else
+        selection = @game.free_positions[rand(@game.free_positions.length)]
+    
+    end
+      
+    puts "\n#{@name} selects #{selection}"
+    selection.to_i
   end
 end
 
 
 
+
 Game.new("computer").play
-puts 
-Game.new("human").play
